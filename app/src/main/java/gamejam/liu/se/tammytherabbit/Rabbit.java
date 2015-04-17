ba7257e4f934bfcfc1066395a2acbdc0d23ec47a
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-04-17 23:34:15 +0200

    Now actually a rabbit, or part of it.

---
+++ app/src/main/java/gamejam/liu/se/tammytherabbit/Rabbit.java
@@ -0,0 +1,12 @@
+package gamejam.liu.se.tammytherabbit;
+
+import android.util.Log;
+
+/**
+ * Created by rovanion on 2015-04-17.
+ */
+public class Rabbit {
+    public static void onClick(){
+        Log.d("Bleh", "Push da batton!");
+    }
+}
